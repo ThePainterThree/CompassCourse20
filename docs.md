c43a81e771a71e3cc40ca3f07e76594d2f3891aa
Patricia <marie.lou.trips@gmail.com>
2022-12-07 09:09:47 +0000

    Clean Code

---
+++ docs.md
@@ -0,0 +1,11 @@
+# Project Documentation (md = mark down)
+### Color Identities
+* CFF5E7
+* A0E4CB
+* 59C1BD
+* FAAB78
+### Color Variables
+* --bg: CFF5E7 (background color)
+* --fg: 59C1BD (Font color)
+* --pC: FAAB78 (primary Color)
+* --sC: A0E4CB (secondary Color)
---
+++ scripts/backend.js
@@ -0,0 +1,29 @@
+// Datatypes in computer languages
+var text = "Hello World"; //String
+var number = 42 //Integer
+var char = "@" //Characters
+var degree = 50.16 //float
+var isHungry = false //Boolean false or true
+var students = [
+    "Enzo",
+    "Jenny",
+    "Patricia",
+    "Hannatu",
+    "Ivan",
+    "Jessica",
+    "Lata"
+]
+
+//Backend Commands
+console.log("Hello World");
+console.warn(text + " " + degree + " " + isHungry);
+console.info(students[2]) + [ ] + [6]
+
+//Frontend Commands
+//alert("The answer for Everything is number" + number)
+
+//TODOLIST FUNCTIONS
+1 usage
+function AddToDo(){
+    console.log("Started the Add ToDo Function");
+}
